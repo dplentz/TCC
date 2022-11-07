d5341dc6a1acf5d1d176fd73eeb4bb6ce715c712
Maria Eduard <dudaplentz123@gmail.com>
2022-11-07 12:22:08 -0300

    perfil

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { View } from 'react-native';
 import { Layout, Text } from 'react-native-rapi-ui';
-
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  FlatList,
+  MeuEstiloheet,
+  StatusBar,
+} from "react-native";
+import user from "../navigation/AppNavigator";
+import { render } from 'react-dom';
+ 
 export default function ({ navigation }) {
-	return (
-		<Layout>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Text>This is the Profile tab</Text>
-			</View>
+    this.state = { 
+      nome: user.nome,
+	  email: user.email,
+	  genero: user.genero,
+      uid: user.uid,
+	  dataNasc: user.dataNasc,
+    }  
+    return (
+	 <Layout>
+    <View>
+        <Text>
+          {this.state.nome} ,oi
+		  {this.state.email},
+		  {this.state.genero},
+		  {this.state.dataNasc}
+        </Text>
+      </View>
 		</Layout>
 	);
-}
+	
+};
+
